be6a7c4efbd7ec096bbcaa860ab3d816cd6b2c86
ricardowrk7 <ricardowrk7@gmail.com>
2022-01-24 23:08:29 +0330

    basket was created and redux was installed

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import styles from "./Header.module.css";
+import { ShoppingBagIcon } from "@heroicons/react/solid";
+import Link from "next/link";
 
 interface Props {}
 
@@ -26,15 +28,21 @@ const Header = (props: Props) => {
           header ? "bg-main" : "bg-lime-100"
         } flex justify-center sticky top-0 z-10`}
       >
-        <div className={` container flex py-6`}>
+        <div className={` container flex py-6 items-center`}>
           <div>logo</div>
           <div className="flex flex-1">
             <nav className="w-full">
-              <ul className="flex justify-end w-full">
+              <ul className="flex justify-end items-center w-full">
                 <li className="mr-6">signup</li>
                 <li className="mr-6">login</li>
                 <li className="mr-6">order</li>
-                <li>basket</li>
+                <li><Link href="/basket">
+                  <span className="relative">
+                  <ShoppingBagIcon className="h-8 w-8" />
+                  <span className="absolute -bottom-4 left-6 bg-red-600 text-white  w-5 h-5 flex justify-center items-center rounded-full    ">2</span>
+                  </span>
+                  
+                  </Link></li>
               </ul>
             </nav>
           </div>
