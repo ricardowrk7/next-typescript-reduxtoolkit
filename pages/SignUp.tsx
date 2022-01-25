726ad966a7b186c1b8b385aae6911f0c4cd40227
ricardowrk7 <ricardowrk7@gmail.com>
2022-01-25 05:27:55 +0330

    initialvalues and validationscema was defined

--- pages/SignUp.tsx
+++ pages/SignUp.tsx
@@ -1,5 +1,28 @@
-
+import React, { useState,useEffect } from "react";
+import { useFormik} from "formik";
+import * as Yup from "yup";
 const SignUp=()=>{
+
+    const { handleChange, handleBlur, values, errors, touched, isValid } =
+    useFormik<{
+      name: string;
+      link: string;
+      id: string;
+    }>({
+      initialValues: {
+        name: "",
+        link: "",
+        id: "",
+      },
+      validateOnMount:true,
+      onSubmit: (): void => {},
+      validationSchema: Yup.object().shape({
+        name: Yup.string().min(3).max(20).required("is required"),
+        link: Yup.string().min(10).max(250).required("is required"),
+
+        id: Yup.string().required("is required"),
+      }),
+    });
     return (
 
     )
