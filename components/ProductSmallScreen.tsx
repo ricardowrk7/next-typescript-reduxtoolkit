eb9ab0d7c26c14cc55ba8d162d737568b7be2c8e
ricardowrk7 <ricardowrk7@gmail.com>
2022-01-28 13:33:10 +0330

    addToCart was added to productSmallScreen

--- components/ProductSmallScreen.tsx
+++ components/ProductSmallScreen.tsx
@@ -1,10 +1,26 @@
 import { ShoppingCartIcon } from "@heroicons/react/solid";
 import Image from "next/image";
+import { addToCart } from "../redux/basketSlice";
+import { useAppDispatch } from "../useHook/useReduxHook";
 
 type Props={
     foodOptions:any
 }
 const ProductSmallScreen=({foodOptions}:Props)=>{
+  const dispatch = useAppDispatch();
+  const addToCartHandler = (item: any) => {
+    console.log(item);
+    dispatch(
+      addToCart({
+        name: item.name,
+        category: item.categories[0].slug,
+        price: item.price.raw,
+        qty: 1,
+        id: item.id,
+        src: item.media.source,
+      })
+    );
+  };
     return(
         <div className="flex Flipped my-5  overflow-x-auto px-3 py-5 space-x-5">
         {foodOptions?.map((item: any) => {
@@ -23,7 +39,7 @@ const ProductSmallScreen=({foodOptions}:Props)=>{
                 <p className="text-sm mt-2" dangerouslySetInnerHTML={{ __html: item.description }} />
                 <div className="flex justify-between mt-2">
                   <p className="text-semibold">{item.categories[0].slug}</p>
-                  <div className="border-2 py-1 px-2 cursor-pointer group hover:bg-yellow-400 flex justify-center space-x-4 rounded-md border-yellow-400 ">
+                  <div  onClick={() => addToCartHandler(item)} className="border-2 py-1 px-2 cursor-pointer group hover:bg-yellow-400 flex justify-center space-x-4 rounded-md border-yellow-400 ">
                     <span className="text-gray-700 group-hover:text-teal-900">
                       add to cart
                     </span>
--- pages/Basket.tsx
+++ pages/Basket.tsx
@@ -6,51 +6,86 @@ import { MinusIcon } from "@heroicons/react/solid";
 import { decreaseQty, increaseQty } from "../redux/basketSlice";
 import Header from "../components/Header";
 import Link from "next/link";
-
+import { useRouter } from "next/router";
 
 const Basket = () => {
- 
-
+  const router = useRouter();
   const state = useAppSelector((state) => state.basket);
-  const dispatch=useAppDispatch()
-  const increaseQtyHandler=(product:basketItem)=>{
-      dispatch(increaseQty(product))
-  }
 
-  const decreaseQtyHandler=(product:basketItem)=>{
-    dispatch(decreaseQty(product))
-  }
+  const userToken = useAppSelector((state) => state.user.userInfo.token);
+  const dispatch = useAppDispatch();
+  const increaseQtyHandler = (product: basketItem) => {
+    dispatch(increaseQty(product));
+  };
+
+  const decreaseQtyHandler = (product: basketItem) => {
+    dispatch(decreaseQty(product));
+  };
 
   return (
-  <div>
-      <Header bg={false}  />
-        <div className="grid px-16 my-5  py-5 gap-4 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {state.cartItems?.length!>0? state.cartItems?.map((product: basketItem) => {
-        return (
-          <div key={product.id} className="rounded  overflow-hidden">
-            <div className="w-full">
-              <Image width={400} height={220} src={product.src} />
-            </div>
-            <div>
-              <p className="font-bold">{product.name}</p>
-              <p className="mt-2">qty*price: ${product.qty * product.price}</p>
-              <div className="flex mt-3 space-x-5">
-                <span onClick={()=>decreaseQtyHandler(product)} className="border-yellow-400 cursor-pointer border-2 px-2 flex items-center rounded">
-                  <MinusIcon className="w-5 h-5 " />
-                </span>
-                <span className="font-semibold">{product.qty}</span>
-                <span onClick={()=>increaseQtyHandler(product)} className="border-yellow-400 border-2 px-2 flex items-center rounded cursor-pointer">
-                  <PlusIcon className="w-5 h-5 " />
-                </span>
+    <div>
+      <Header bg={false} />
+      <div className="grid px-6 sm:px-16 my-5  py-5 gap-4 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {state.cartItems?.length! > 0 ? (
+          state.cartItems?.map((product: basketItem) => {
+            return (
+              <div key={product.id} className="rounded  overflow-hidden">
+                <div className="w-full">
+                  <Image width={400} height={220} src={product.src} />
+                </div>
+                <div>
+                  <p className="font-bold">{product.name}</p>
+                  <p className="mt-2">
+                    qty*price: ${product.qty * product.price}
+                  </p>
+                  <div className="flex mt-3 space-x-5">
+                    <span
+                      onClick={() => decreaseQtyHandler(product)}
+                      className="border-yellow-400 cursor-pointer border-2 px-2 flex items-center rounded"
+                    >
+                      <MinusIcon className="w-5 h-5 " />
+                    </span>
+                    <span className="font-semibold">{product.qty}</span>
+                    <span
+                      onClick={() => increaseQtyHandler(product)}
+                      className="border-yellow-400 border-2 px-2 flex items-center rounded cursor-pointer"
+                    >
+                      <PlusIcon className="w-5 h-5 " />
+                    </span>
+                  </div>
+                </div>
               </div>
-            </div>
+            );
+          })
+        ) : (
+          <div>
+            <p className="font-bold  text-base sm:text-lg text-gray-500 mb-4">
+              there isnt any item in your basket
+            </p>
+            <span className="text-blue-500 font-semibold">
+              <Link href="/">click here to go home page</Link>
+            </span>
           </div>
-        );
-      }):<div><p className="font-bold text-lg text-gray-500 mb-4">there isnt any item in your basket</p>
-       <span className="text-blue-500 font-semibold"><Link href="/">click here to go home page</Link></span>
-      </div>}
+        )}
+      </div>
+      <div className=" flex my-5 justify-center sm:justify-start container mx-auto px-4 py-4">
+        {userToken ? (
+          <button
+            onClick={() => router.push({ pathname: "/Checkout" })}
+            className="bg-yellow-400 px-8 py-2 rounded"
+          >
+            checkout
+          </button>
+        ) : (
+          <button
+            onClick={() => router.push({ pathname: "/SignUp" })}
+            className="bg-yellow-400 px-8 py-2 rounded"
+          >
+            checkout
+          </button>
+        )}
+      </div>
     </div>
-  </div>
   );
 };
 
